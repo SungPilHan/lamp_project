8f473f4b3b7524622334c3b8ea072e1f19a7e75d
SungPilHan <ttt16541@gmail.com>
2018-06-28 10:41:50 +0900

    interface 수정

--- Src/module/interface.py
+++ Src/module/interface.py
@@ -1,40 +1,48 @@
-from Tkinter import *
+from tkinter import *
 
 class Interface():
-
     def __init__(self):
         self.tk = Tk()
         self.tk.title("lamp_program")
-        self.tk.geometry("640x400+100+100")
-        self.tk.resizable(False, False)
+        self.tk.geometry("320x100")
         self.imgOff = PhotoImage(file='light_bulb_off.gif')
         self.imgOn = PhotoImage(file='light_bulb_on.gif')
+        self.labels = []
+
+        for k in range(0,5):
+            label = Label(image=self.imgOff)
+            label.pack(side=LEFT)
+            self.labels.append(label)
 
         for k in range (0,5):
-            self.bind(k+1, self.userInput)
+            self.bind(str(k+1), self.userInput)
 
-
+        self.tk.mainloop()
 
     def getChoice(self):
         return self.choice
 
     def userInput(self,event):
-        self.choice = event.getchar()
+        self.choice = event.char()
 
     def drawLamp(self, lamps):
+        for label in self.labels:
+            label.pack_forget()
+            del label
 
         for lamp in lamps:
             # turn on
             if lamp.getState():
                 label = Label(image=self.imgOn)
-                label.image = self.imgOn
-                label.pack(side=LEFT, padx=10, pady=10)
+                label.pack(side=LEFT)
+                self.labels.append(label)
 
             # turn off
             else:
                 label = Label(image=self.imgOff)
-                label.image = self.imgOff
-                label.pack(side=LEFT, padx=10, pady=10)
+                label.pack(side=LEFT)
+                self.labels.append(label)
 
         self.tk.update()
 
+Interface()
