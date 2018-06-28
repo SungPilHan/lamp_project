cbaacde33eebaeba89b0a51223f8ad5ae5c56a92
201311105 <93kjh1105@daum.net>
2018-06-28 10:32:37 +0900

    module update
    controller module. control method update
    while 제거, drawLamp함수 인자 추가

--- Src/module/controller.py
+++ Src/module/controller.py
@@ -2,14 +2,14 @@ class controller(object):
 	def __init__(self, lamps):
 		self.lamps = lamps
 		self.interface = Interface()
+	
 	def control(self):
-		while(True):
-			choice = interface.getChoice()
-			lamp = lamps[int(choice) - 1]
-			
-			if lamp.getState():
-				lamp.turnOff()
-			else:
-				lamp.turnOn()
+		choice = interface.getChoice()
+		lamp = lamps[int(choice) - 1]
+		
+		if lamp.getState():
+			lamp.turnOff()
+		else:
+			lamp.turnOn()
 
-			interface.drawLamp()
+		interface.drawLamp(lamp)
